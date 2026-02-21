1dd3b653b63fb5117d229f32b1e3a791a8a6b06a
Blinkflo Builder <builder@blinkflo.dev>
2026-02-21 12:04:28 +0000

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,54 @@
+
+import HeroSection from "./heroSection";
+import TargetAudienceSectionComp from "./targetAudienceSection";
+import GridVideo from "./GridVideo";
+import WhyChoose from "./whyChoose";
+import FeaturesSection from "./featuresSection";
+import TransformSection from "./TransformSection";
+import MetricSection from "./key-metric";
+import IntegrationSection from "./integrationsSection";
+import PricingSection from "./PricingSection";
+import FaqSection from "./FaqSection";
+import Testimonial from "./testimonial";
+import AboutHeroSection from "./aboutHeroSection";
+import AboutMetric from "./aboutMetric";
+import AboutOurStory from "./aboutOurStory";
+import AboutOurTeam from "./aboutOurTeam";
+import AboutOurValues from "./aboutOurValues";
+import SectionHeader from "./sectionHeader";
+import ContactSupportOptions from "./contactSupportOptions";
+import ContactOurOffices from "./ContactOurOffices";
+import CareersOpenPosition from "./careersOpenPosition";
+import CareerWhyWork from "./careerWhyWork";
+import BlogsCard from "./blogsCard";
+import OurServices from "./ourServices";
+import Header from "./header";
+import Footer from "./footer";
+
+export const componentRegistry = {
+  "hero-section": HeroSection,
+  "target-audience": TargetAudienceSectionComp,
+  "grid-video": GridVideo,
+  "why-choose": WhyChoose,
+  "feature-section": FeaturesSection,
+  "transform-section": TransformSection,
+  "metrics-section": MetricSection,
+  "integration-section": IntegrationSection,
+  "pricing-section": PricingSection,
+  "faq-section": FaqSection,
+  "testimonials-section": Testimonial,
+  "about-hero": AboutHeroSection,
+  "about-metric": AboutMetric,
+  "about-our-story": AboutOurStory,
+  "about-our-team": AboutOurTeam,
+  "about-our-values": AboutOurValues,
+  "section-header": SectionHeader,
+  "contact-support-options": ContactSupportOptions,
+  "contact-our-offices": ContactOurOffices,
+  "careers-open-positions": CareersOpenPosition,
+  "career-why-work": CareerWhyWork,
+  "blogs-card-section": BlogsCard,
+  "our-services": OurServices,
+  "header-sec": Header,
+  "footer-section": Footer
+};
